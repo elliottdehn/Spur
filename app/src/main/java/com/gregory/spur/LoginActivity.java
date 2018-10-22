38c938c7b552dbcebdd4341259ee238b55383655
loganFry <fry.348@osu.edu>
2018-10-22 14:35:49 -0400

    Launch maps activity after login, fix api key issue

--- app/src/main/java/com/gregory/spur/LoginActivity.java
+++ app/src/main/java/com/gregory/spur/LoginActivity.java
@@ -3,6 +3,7 @@ package com.gregory.spur;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -109,7 +110,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private void updateUI(@Nullable FirebaseUser user){
         if(user != null){
-            Toast.makeText(getApplicationContext(), "Logged in user: " + user.getEmail(), Toast.LENGTH_LONG).show();
+            // If the login was successful, launch the maps activity
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
         }
     }
 
