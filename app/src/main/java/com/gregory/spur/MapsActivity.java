e1603c84022d1df16ade3433cf6440d4f244c06f
loganFry <fry.348@osu.edu>
2018-10-23 16:57:07 -0400

    Draw markers for current events on map

--- app/src/main/java/com/gregory/spur/MapsActivity.java
+++ app/src/main/java/com/gregory/spur/MapsActivity.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -19,22 +20,31 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.gregory.spur.domain.Event;
+import com.gregory.spur.services.EventService;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnCompleteListener<QuerySnapshot>, LocationListener {
 
 
     private GoogleMap mMap;
     private LocationManager locationManager;
+    private EventService mEventService;
     private String TAG="SPURDebug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mEventService = new EventService();
 
         setContentView(R.layout.activity_maps);
         //Button button= (Button)findViewById(R.id.button);
@@ -59,84 +69,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // if the network provider is available
         if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
 
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double lat= location.getLatitude();
-                    double lon= location.getLongitude();
-                    LatLng latlng=new LatLng(lat,lon);
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        List<Address> addressList= geocoder.getFromLocation(lat,lon,1);
-                        String str =addressList.get(0).getLocality();
-                        str +=addressList.get(0).getCountryName();
-                        mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in "+ str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10.8f));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            });
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
 
         }
         else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
 
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double lat= location.getLatitude();
-                    double lon= location.getLongitude();
-                    LatLng latlng=new LatLng(lat,lon);
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        List<Address> addressList= geocoder.getFromLocation(lat,lon,1);
-                        String str =addressList.get(0).getLocality();
-                        str +=addressList.get(0).getCountryName();
-                        mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in "+ str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10.8f));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            });
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         }
 
+        // Display the logged in user
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         Toast.makeText(getApplicationContext(), "Logged in user: " + currentUser.getEmail(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat= location.getLatitude();
+        double lon= location.getLongitude();
+        LatLng latlng=new LatLng(lat,lon);
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+        try {
+            List<Address> addressList= geocoder.getFromLocation(lat,lon,1);
+            String str =addressList.get(0).getLocality();
+            str +=addressList.get(0).getCountryName();
+            mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in "+ str));
+            mEventService.getEvents(this);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10.8f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
     @Override
@@ -177,12 +154,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.d(TAG, "onDestroy() has been called");
     }
 
+    @Override
+    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+        if(task.isSuccessful()){
+            for (QueryDocumentSnapshot document : task.getResult()){
+                Event event = document.toObject(Event.class);
+                double lat = event.getLocation().getLatitude();
+                double lng = event.getLocation().getLongitude();
+                String title = event.getTitle();
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(new LatLng(lat, lng))
+                                .title(title));
+            }
+        } else {
+            Log.e(TAG, "Error getting documents: ", task.getException());
+        }
+    }
+
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -190,6 +184,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mEventService.getEvents(this);
     }
 }
