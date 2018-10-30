1c6741cbc669849e93a8f2813fa3cfe7843c9699
loganFry <fry.348@osu.edu>
2018-10-30 17:15:23 -0400

    Add CreateUserActivity and set initial layout

---
+++ app/src/main/java/com/gregory/spur/CreateUserActivity.java
@@ -0,0 +1,56 @@
+package com.gregory.spur;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.gregory.spur.domain.User;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    private static final String EXTRA_AUTH_ID = "auth_id";
+    private static final String EXTRA_USER_ID = "user_id";
+
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mUserName;
+    private EditText mAge;
+    private EditText mCity;
+    private RadioButton mMale;
+    private RadioButton mFemale;
+
+    private String mAuthId;
+    private String mUserId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+
+        mAuthId = getIntent().getStringExtra(EXTRA_AUTH_ID);
+        mUserId = getIntent().getStringExtra(EXTRA_USER_ID);
+
+        mFirstName = findViewById(R.id.first_name_edittext);
+        mLastName = findViewById(R.id.last_name_edittext);
+        mUserName = findViewById(R.id.username_edittext);
+        mAge = findViewById(R.id.age_edittext);
+        mCity = findViewById(R.id.city_edittext);
+        mMale = findViewById(R.id.male_radio_button);
+        mFemale = findViewById(R.id.female_radio_button);
+    }
+
+    public static Intent newIntent(Context packageContext, String authId, String userId) {
+        Intent intent = new Intent(packageContext, CreateUserActivity.class);
+        intent.putExtra(EXTRA_AUTH_ID, authId);
+        intent.putExtra(EXTRA_USER_ID, userId);
+        return intent;
+    }
+
+    private User readFromUI(){
+        return new User();
+    }
+}
